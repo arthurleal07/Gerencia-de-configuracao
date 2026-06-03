6ff581473284716137beb0797e98bc9a4d4194e3
arthurleal07 <arthurvinicius.ifam@gmail.com>
2026-06-03 10:34:37 -0400

    feat: adiciona função de soma

---
+++ soma.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Soma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("\nResultado: " + numero1 + " + " + numero2 + " = " + resultado);
+
+        scanner.close();
+    }
+}
